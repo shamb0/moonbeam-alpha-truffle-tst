ff83a77bb08754e94db078c0f96be40d354816ff
shamb0 <r.raajey@gmail.com>
2020-10-23 14:18:14 +0530

    sample test-01

---
+++ truffle-tst1/migrations/1_initial_migration.js
@@ -0,0 +1,6 @@
+const Migrations = artifacts.require("Migrations");
+
+module.exports = function(deployer) {
+    // deployer.deploy(Migrations, { gas: 4294967295 });
+  deployer.deploy(Migrations);
+};
